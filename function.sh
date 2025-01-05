13c6d424d9684a988e653782fbfb569428c89ae9
kjvinodraj <kjvinodraj@gmail.com>
2025-01-05 09:32:37 +0530

    added new file

---
+++ function.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 failure"
+    else
+        echo "$2 success"
+}
+
+dnf list installed mysql-community-server
+VALIDATE $? "mysql server "
+
+sudo dnf install mysql-server
+
+VALIDATE $? "mysql installation"
+
+
+
+
