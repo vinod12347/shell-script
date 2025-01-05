c65a4e4e96a1dafb087c9d309da81b6439fe4af0
kjvinodraj <kjvinodraj@gmail.com>
2025-01-05 09:40:27 +0530

    updated script

--- function.sh
+++ function.sh
@@ -11,12 +11,25 @@ VALIDATE(){
     fi
 }
 
+if [ $USERID -ne 0 ]
+then
+    echo "user must have sudo access"
+    exit 1
+fi
+
 dnf list installed mysql-community-server
-VALIDATE $? "mysql server "
 
-sudo dnf install mysql-server
-
+if [ $? -ne 0 ]
+then 
+VALIDATE $? "mysql server alreday installed"
+else
+sudo dnf install mysql-server -y
+if [ $? -ne 0 ]
+then
+  VALIDATE $? "mysql installation"
+else
 VALIDATE $? "mysql installation"
+fi
 
 
 
