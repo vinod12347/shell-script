8469d8b35bbd84e5bdc513d78dc598c051573a3e
kjvinodraj <kjvinodraj@gmail.com>
2025-01-05 16:38:07 +0530

    updated script

--- log1.sh
+++ log1.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/shellscript-log"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
